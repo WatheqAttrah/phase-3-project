dada2cc662dc3f89dc8f26e48d97dfb8959beef7
Watheq Attrah <novaatrah@gmail.com>
2023-08-30 13:52:39 -0700

    Add export_as_csv function to cli

--- lib/cli.py
+++ lib/cli.py
@@ -8,7 +8,8 @@ def main():
         print("Menu: ")
         print("1. View Cars")
         print("2. Delete Car")
-        print("3. Exit")
+        print("3. Export as csv file")
+        print("4. Exit")
 
         option = input("Enter Your Option: ")
         if option == "1":
@@ -16,6 +17,8 @@ def main():
         elif option == "2":
             delete_car()
         elif option == "3":
+            export_as_csv()                        
+        elif option == "4":
             print("Exit Application")
             break
 
