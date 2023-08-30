b2ab1b85f4fce93b3a7dc4e81b18813d3815277c
Watheq Attrah <novaatrah@gmail.com>
2023-08-30 13:32:42 -0700

    Add delete_car option to cli

--- lib/cli.py
+++ lib/cli.py
@@ -2,20 +2,23 @@
 
 from helpers import view_cars
 
+
 def main():
     while True:
         print("Menu: ")
         print("1. View Cars")
-        print("2. Exit")
-        
-        option = input('Enter Your Option: ')
+        print("2. Delete Car")
+        print("3. Exit")
+
+        option = input("Enter Your Option: ")
         if option == "1":
             view_cars()
         elif option == "2":
+            delete_car()            
+        elif option == "3":
             print("Exit Application")
             break
-    
-    
+
+
 if __name__ == "__main__":
     main()
-
